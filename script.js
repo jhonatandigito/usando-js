6c1b5ce833cbbd8cdaf9594938d73ebcd8e65ec9
jhonatandigito <102267615+jhonatandigito@users.noreply.github.com>
2022-07-14 10:39:27 -0300

    add exe01 , script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+var area = document.getElementById("area");
+
+function entrar() {
+  var nome = prompt("Digite seu nome");
+
+  if (nome === "" || nome === null) {
+    alert("Ops algo deu errado");
+    area.innerHTML = "Clique no botão para acessar";
+  } else {
+    area.innerHTML = "Bem vindo! " + nome + "" ;
+
+    let botaoSair = document.createElement("button");
+    botaoSair.innerHTML = " Sair da conta ";
+    botaoSair.onclick = sair;
+    area.appendChild(botaoSair);
+  }
+}
+
+function sair() {
+  alert("Até mais!");
+  area.innerHTML = "Você saiu!";
+}
